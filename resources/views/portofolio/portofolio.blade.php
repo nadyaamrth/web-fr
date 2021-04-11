c954be433ccf9e53bd499494b317511e5eadb5b0
Nadyaamrth <nadya.ny82@gmail.com>
2021-04-11 13:09:08 +0700

    portofolio

---
+++ resources/views/portofolio/portofolio.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portofolio</title>
+    <link rel="stylesheet" href="/css/portofolio.css">
+</head>
+
+<body style="background: url(/img/nature2.jpg); background-size: cover; background-attachment: fixed;">
+    <div class="bio">
+        <h1 align="center"><span>Biodata Diri</span></h1>
+        <h2 align="center" style="margin-top: -20px;">Tentang <span>Saya</span></h2>
+        <table>
+            <tr>
+                <td rowspan="17" width="100px" style="vertical-align: top"><img src="/img/187020.jpg" alt="" width="200px" style="display: block; 
+                border-radius: 5%; border-color: white; margin-right: 30px; border: 2px;"></td>
+            </tr>
+            <tr>
+                <td><b>Nama</b></td>
+                <td>:</td>
+                <td>Nadya Margarreth</td>
+            </tr>
+            <tr>
+                <td><b>NPM</b></td>
+                <td>:</td>
+                <td>1855061001</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>Tempat, Tanggal Lahir</b></td>
+                <td>:</td>
+                <td>Bandar Lampung, 6 Maret 2000</td>
+            </tr>
+            <tr>
+                <td><b>Alamat</b></td>
+                <td>:</td>
+                <td>Way Halim, Bandar Lampung</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>No. Telepon</b></td>
+                <td>:</td>
+                <td>082175293334</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>Program Studi</b></td>
+                <td>:</td>
+                <td>Teknik Informatika</td>
+            </tr>
+            <tr>
+                <td><b>Fakultas</b></td>
+                <td>:</td>
+                <td>Teknik</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>Riwayat Pendidikan</b></td>
+                <td>:</td>
+                <td>SDN 2 Perumnas Way Halim 2006-2012</td>
+            </tr>
+            <tr>
+                <td rowspan="3"></td>
+                <td rowspan="3"></td>
+                <td>SMP Kartika II-2 Bandar Lampung 2012-2015</td>
+            </tr>
+            <tr>
+
+                <td>SMA YP Unila Bandar Lampung 2015-2018</td>
+            </tr>
+            <tr>
+
+                <td>S1 Teknik Informatika Universitas Lampung 2018-sekarang</td>
+            </tr>
+            <tr>
+                <td style="white-space: nowrap"><b>Daftar Matkul Fav</b></td>
+                <td>:</td>
+                <td>UI/UX</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>Rencana Judul TA</b></td>
+                <td>:</td>
+                <td>Sistem Untuk Course Coding</td>
+            </tr>
+            <tr>
+                <td><b>Konsep TA</b></td>
+                <td>:</td>
+                <td>Sistem yang ingin dibuat berupa kursus online terbuka yang bekerja sama 
+                    dengan universitas dan organisasi lain untuk menawarkan kursus online 
+                    untuk pengembang perangkat lunak, administrator TI, dan personal kreatif web.
+                </td>
+            </tr>
+            <tr>
+                <td><b>Email</b></td>
+                <td>:</td>
+                <td>nadya.margarreth1001@students.unila.ac.id</td>
+            </tr>
+            <tr style="white-space: nowrap">
+                <td><b>Link Github</b></td>
+                <td>:</td>
+                <td><a href="https://github.com/nadyaamrth">nadya margarreth</a></td>
+            </tr>
+        </table>
+    </div>
+    <div class="bio" align="center">
+        <h3 align="center">Sosial <span>Media</span></h3>
+        <a class="icon" href="https://instagram.com/nadyaamrth"><img src="/img/ig.png" height="50px" alt=""></a>
+        <a class="icon" href="https://www.facebook.com/nadya.margarreth/"><img src="/img/fb.png" height="50px"
+                alt=""></a>
+        <a class="icon" href="http://line.me/ti/p/~nadyamrgrth"><img src="/img/line2.png" height="50px" alt=""></a>
+        <a class="icon" href="https://twitter.com/ndyaamrth"><img src="/img/twt.png" height="50px" alt=""></a>
+    </div>
+    <footer align="center" style="color: white;">
+        <i><b>Copyright © <a href="https://github.com/nadyaamrth" style="color: yellow">Nadya Margarreth</a> 2021 <br>
+                Dibuat oleh Nadya Margarreth</b></i>
+    </footer>
+</body>
+
+</html>
